7e6c7d67604cee49e4c95781e37cbdd09dd16aa7
Nicolas <nrivas2017@alu.uct.cl>
2019-09-04 22:33:38 -0400

    Scrip para extraer los temas de las tramitaciones, boletines 400-12800

--- temas.py
+++ temas.py
@@ -1,9 +1,9 @@
 import requests
 import xml.etree.ElementTree as ET
 
-x=8609
+x=400
 Temas=[]
-for i in range(10):
+for i in range(400,12800):
     URL="https://www.senado.cl/wspublico/tramitacion.php?boletin="+str(x)
     response = requests.get(URL)
     nombre="Tramitaciones/tramitacion_"+str(x)+".xml"
@@ -17,6 +17,10 @@ for i in range(10):
             Temas.append(root[0][10][i][0].text.encode("UTF-8"))
     print x
     x+=1
-print Temas
+dTemas=open("Temas.txt","w")
+sTexto=""
+for i in Temas:
+    sTexto+=i+", "
+dTemas.write(sTexto)
 
 
