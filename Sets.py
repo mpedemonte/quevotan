a9bba5178e8d7e9e1b75f53ddbfccbecdf7eab96
ringler0 <31633100+ringler0@users.noreply.github.com>
2019-09-24 15:31:11 -0300

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -1,89 +1,91 @@
-Economia_fomento_y_turismo = {'turismo','deudas','deuda','corredor','propiedades','productividad',
-'emprendimiento','crédito','indemnización','inversión','I.P.C','precio','isapres','isapre','economia',
-'fomento','tarifas','tarifa','banco'}
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+Economia_fomento_y_turismo = {turismo,deudas,deuda,corredor,propiedades,productividad,
+emprendimiento,credito,indemnizacion,inversion,I.P.C,precio,isapres,isapre,economia,
+fomento,tarifas,tarifa,banco}
 
-Desarrollo_social = {'Igualdad','inclusión','discapacidad','desarrollo','sostenible','capacitación'}
+Desarrollo_social = {Igualdad,inclusion,discapacidad,desarrollo,sostenible,capacitacion}
 
-cultura_y_Educación = {'educación','educacional','educacionales','básica','media','prebásica','parvularia','parvularias',
-'parvulo','escuela','escuelas','colegios','colegio','curricular','aprendizaje','cultura','culturales','establecimiento', 'establecimientos','docente','apoderado','apoderados','alumno','alumna','alumnos','alumnas','arancel','titulo',
-'académico','estudiante','estudiantes','evaluación','evaluaciones','enseñanza','programa','programas',
-'asignatura','asignaturas','docentes','subvención','escolar','profesor','profesores','infantil','integración','integra','artes',
-'artisticos','artisticas','artistico','artistica','cultura','culturización','JUNAEB','INJUB','JUNJI'}
+cultura_y_Educacion = {educacion,educacional,educacionales,basica,media,prebasica,parvularia,parvularias,
+parvulo,escuela,escuelas,colegios,colegio,curricular,aprendizaje,cultura,culturales,establecimiento, establecimientos,docente,apoderado,apoderados,alumno,alumna,alumnos,alumnas,arancel,titulo,
+academico,estudiante,estudiantes,evaluacion,evaluaciones,ensenanza,programa,programas,
+asignatura,asignaturas,docentes,subvencion,escolar,profesor,profesores,infantil,integracion,integra,artes,
+artisticos,artisticas,artistico,artistica,cultura,culturizacion,JUNAEB,INJUV,JUNJI}
 
-justicia_y_derechos_humanos = {'nacionalidad','derecho','derechos','humano','humanos','gracia','cuidadano',
-'cuidadanía','sufragio','prisión','sentencia','protección','indigenas','etnia','etnias','indigena','migración',
-'migraciones','extrangería','civil','identificación','mortinatos','manifestaciones','migrantes','penas','carcel',
-'libertad','humanitarias','humanitarios','humanitaria','humanitario','privación','privativas','registro','revoca','comunidades'}
+justicia_y_derechos_humanos = {nacionalidad,derecho,derechos,humano,humanos,gracia,cuidadano,
+cuidadania,sufragio,prision,sentencia,proteccion,indigenas,etnia,etnias,indigena,migracion,
+migraciones,extrangeria,civil,identificacion,mortinatos,manifestaciones,migrantes,penas,carcel,
+libertad,humanitarias,humanitarios,humanitaria,humanitario,privacion,privativas,registro,revoca,comunidades}
 
-trabajo_y_previsión_social ={'beneficio','social','normas','laborales','laboral','beneficios','sociales','biodiversidad',
-'sindicato','sindicatos','sindicales','estatuto','estatutos','código','trabajo','isapre','tutela','consejo',
-'dia','nacional','feriado','procedimiento','faenas','faena'}
+trabajo_y_prevision_social ={beneficio,social,normas,laborales,laboral,beneficios,sociales,biodiversidad,
+sindicato,sindicatos,sindicales,estatuto,estatutos,codigo,trabajo,isapre,tutela,consejo,
+dia,nacional,feriado,procedimiento,faenas,faena}
 
-obras_publicas = {'localidad','comuna','ruta','vía','pública','vías','peaje','sanitario','autopista','autopistas',
-'comunas','localidades','rutas','peajes','país','obras','obra','concesiones','infraestructura','planificación',
-'relicitaciones','vial','hidráulica','región','regiones','rural','rurales','licitación','licitaciones','calle','calles'}
+obras_publicas = {localidad,comuna,ruta,via,publica,vias,peaje,sanitario,autopista,autopistas,
+comunas,autopista,localidades,rutas,peajes,pais,obras,obra,concesiones,infraestructura,planificacion,
+relicitaciones,vial,hidraulica,region,regiones,rural,rurales,licitacion,licitaciones,calle,calles}
 
-salud = {'nacional','cáncer','enfermedad','enfermedades','desfibrilador','desfibriladores','hospital','hospitales',
-'clinica','clinicas','salud','infraestructura','isapres','precio','isapre','fonasa','sanitario','medicinal','medicina',
-'alimentos','saludable','saludables','atención','discapacidad','discapacidades','preferente','protección','órganos',
-'embarazo','epidemiológicos','epidemiológico','dolor','dolores','oncológicos','clinico','clinicos','sindrome',
-'sindromes','médico','nutrientes','nutriente','GES','física','deporte'}
+salud = {nacional,cancer,enfermedad,enfermedades,desfibrilador,desfibriladores,hospital,hospitales,
+clinica,clinicas,salud,infraestructura,isapres,precio,isapre,fonasa,sanitario,medicinal,medicina,
+alimentos,saludable,saludables,atencion,discapacidad,discapacidades,preferente,proteccion,organos,
+embarazo,epidemiologicos,epidemiologico,dolor,dolores,oncologicos,clinico,clinicos,sindrome,
+sindromes,medico,nutrientes,nutriente,GES,fisica,deporte}
 
-vivienda_y_urbanismo = {'integración','social','urbana','vivienda','viviendas','copropiedad','propiedad','inmueble',
-'propiedad','inmuebles','inmobiliaria','instalación','mantención','funcionamiento','urbanismo','construcción',
-'edificación','regularización','autoconstrucción','obras','obra','villa','vecino','vecinos','comuna','comunas',
-'familia,familias,familiar,juntas,comunitaria,comunitarias,comunitario,comunitarios,comunidad',
-'dirigentes','participación','ciudadana','publica','condominio','condominios','poblaciones','villas','población',
-'avalúo','fiscal'}
+vivienda_y_urbanismo = {integracion,social,urbana,vivienda,viviendas,copropiedad,propiedad,inmueble,
+propiedad,inmuebles,inmobiliaria,instalacion,mantencion,funcionamiento,urbanismo,construccion,
+edificacion,regularizacion,autoconstruccion,obras,obra,villa,vecino,vecinos,comuna,comunas,
+familia,familias,familiar,juntas,comunitaria,comunitarias,comunitario,comunitarios,comunidad,
+dirigentes,participacion,ciudadana,publica,condominio,condominios,poblaciones,villas,poblacion,
+avaluo,fiscal}
 
-agricultura = {'aguas','agua','forestal','forestales','sustentable','bosque','nativo','madera','plagas','plaguicidas',
-'suelo','suelos','tierra','extracción','agricultura','agricultor','agricultores','apícola','actividad','actividades',
-'parque','nacional','parques','elaboración','comecialización','leche','lacteos','SAG','ganado','ganaderos','campo',
-'frutícola','frutícolas','frutas','árboles','árbol','sequía','riego','riegos','poda','podas','cosecha','cosechas','animales',
-'animal','herbicida','herbicidas','plaguicida','fungicida','fungicidas'}
+agricultura = {aguas,agua,forestal,forestales,sustentable,bosque,nativo,madera,plagas,plaguicidas,
+suelo,suelos,tierra,extraccion,agricultura,agricultor,agricultores,apicola,actividad,actividades,
+parque,nacional,parques,elaboraciin,comecializacion,leche,lacteos,SAG,ganado,ganaderos,campo,
+fruticola,fruticolas,frutas,arboles,arbol,sequia,riego,riegos,poda,podas,cosecha,cosechas,animales,
+animal,herbicida,herbicidas,plaguicida,fungicida,fungicidas}
 
-minería_y_energía = {'servicios','eléctricos','eléctrico','distribución','cobre','glaciares','eficiencia','energética',
-'división','mina','fundición','minería','minera','mineral','instalación','empalme','empalmes','eléctricidad','distribución',
-'energético','energético','energía','lumínica','luz','revisión','reparación','medidor','medidores','transmisión'}
+mineria_y_energia = {servicios,electricos,electrico,distribucion,cobre,glaciares,eficiencia,energetica,
+division,mina,fundicion,mineria,minera,mineral,instalacion,empalme,empalmes,electricidad,distribucion,
+energetico,energetico,energia,luminica,luz,revision,reparacion,medidor,medidores,transmision}
 
-transporte_y_telecomunicaciones = {'sistema','automático','automatizado','roaming','carreras','celulares','pantalla',
-'celular','pantallas','conducción','vehículo','vehículos','motor','bencina','bencinas','petróleo','motores','motorizados',
-'motorizado','estacionar','telecomunicación','telecomunicaciones','radio','radiodifusión','bus','buses','trolebuses',
-'radioeléctrico','transporte','transportes','interurbanos','circulación','medios','pasajeros','pasajero','aeronáutica',
-'aviación','avión','aeropuerto','barco','barcos','puerto','puertos','movilidad','red','comunicación','comunicaciones',
-'portuario','tendido','cables','tarifas'}
+transporte_y_telecomunicaciones = {sistema,automatico,automatizado,roaming,carreras,celulares,pantalla,
+celular,pantallas,conduccion,vehiculo,vehiculos,motor,bencina,bencinas,petreleo,motores,motorizados,
+motorizado,estacionar,telecomunicacion,telecomunicaciones,radio,radiodifusion,bus,buses,trolebuses,
+radioelectrico,transporte,transportes,interurbanos,circulacion,medios,pasajeros,pasajero,aeronautica,
+aviacion,avion,aeropuerto,barco,barcos,puerto,puertos,movilidad,red,comunicacion,comunicaciones,
+portuario,tendido,cables,tarifas}
 
-medio_ambiente_y_bienes_nacionales = {'medioambiente','mediomabiental','medioambientales','cambio','clima','climático',
-'industria','industrial','industriales','industrias','alga','alguera','algas','fundición','borde','costero','mar','mares','mareas',
-'marítimas','costa','costas','bordes','fundiciones','PPDA','emisiones','emisión','medio','ambiente','daño','ambiental','ambientales',
-'inversión','humedal','humedales','animales','parques','nacionales','parque','nacional','animal','bosques','bosque','árbol','árboles',
-'río','ríos','lago','lagos','glaciar','glaciares','islas','isla','acuicultura','mina','minas','plástico','plásticos','basura','desechos',
-'degradable','degradación','biodegradable','biodegradables','basuras','desecho','biomasa','emisiones','plásticas','plastica',
-'desechables','desechable','climático','acuícolas','Antártica','camino','caminos','carreteras','carretera','CONAF','plantación',
-'plantaciones','turbera','turberas','áreas','área','silvestre','silvestres','pino','salmonicultura','hídrica','hídrico','recurso',
-'recursos','biósfera','sustentable','sustentables','sustentabilidad','autosustentable','autosustentables','autosustentabilidad',
-'contaminación','nieve','volcán','volcanes','volcánico','volcánicas','volcánica','volcánicos','SERNAPESCA','cetáceos','fauna',
-'marina','bienestar','animal','plantas','forestal','urbanismo','construcciones','SERNAGEOMIN','derrame','derrames','latentes',
-'saturadas','descontaminación'}
+medio_ambiente_y_bienes_nacionales = {medioambiente,mediomabiental,medioambientales,cambio,clima,climatico,
+industria,industrial,industriales,industrias,alga,alguera,algas,fundicion,borde,costero,mar,mares,mareas,
+maritimas,costa,costas,bordes,fundiciones,PPDA,emisiones,emision,medio,ambiente,dano,ambiental,ambientales,
+inversion,humedal,humedales,animales,parques,nacionales,parque,nacional,animal,bosques,bosque,arbol,arboles,
+rio,rios,lago,lagos,glaciar,glaciares,islas,isla,acuicultura,mina,minas,plastico,plasticos,basura,desechos,
+degradable,degradacion,biodegradable,biodegradables,basuras,desecho,biomasa,emisiones,plasticas,plastica,
+desechables,desechable,climatico,acuicolas,Antartica,camino,caminos,carreteras,carretera,CONAF,plantacion,
+plantaciones,turbera,turberas,areas,area,silvestre,silvestres,pino,salmonicultura,hidrica,hidrico,recurso,
+recursos,biosfera,sustentable,sustentables,sustentabilidad,autosustentable,autosustentables,autosustentabilidad,
+contaminacion,nieve,volcan,volcanes,volcanico,volcanicas,volcanica,volcanicos,SERNAPESCA,cetaceos,fauna,
+marina,bienestar,animal,plantas,forestal,urbanismo,construcciones,SERNAGEOMIN,derrame,derrames,latentes,
+saturadas,descontaminacion}
 
-deporte = {'deportes','deporte','física','actividad','actividades','calidad','vida','activo','activos','saludable',
-'saludables','personas','vulnerabilidad','discapacidad','planes','programas','deportiva','deportivo','población','alto','rendimiento',
-'competencia','paralímpica','paralímpico','olimpiadas','participación','talento','talentos','preparación','deportista','deportistas',
-'sedentarismo','obesidad','escolar','escolares','IND'}
+deporte = {deportes,deporte,fisica,actividad,actividades,calidad,vida,activo,activos,saludable,
+saludables,personas,vulnerabilidad,discapacidad,planes,programas,deportiva,deportivo,poblacion,alto,rendimiento,
+competencia,paralimpica,paralimpico,olimpiadas,participacion,talento,talentos,preparacion,deportista,deportistas,
+sedentarismo,obesidad,escolar,escolares,IND}
 
-mujer_y_la_equidad_de_género = {'mujer','enajenación','femicidio','mujeres','equidad','genero','acoso','violación','violencia',
-'FENASENF','sexual','discriminación','libre','femicidios','dicriminación','participación','igualdad','FENPRUSS','vulneración',
-'abuso','maltrato','femicidios','intrafamiliar','sexuales','homosexual','transexual','homosexuales','transexuales','prevenir',
-'prevención'}
+mujer_y_la_equidad_de_genero = {mujer,enajenacion,femicidio,mujeres,equidad,genero,acoso,violacion,violencia,
+FENASENF,sexual,discriminacion,libre,femicidios,dicriminacion,participacion,igualdad,FENPRUSS,vulneracion,
+abuso,maltrato,femicidios,intrafamiliar,sexuales,homosexual,transexual,homosexuales,transexuales,prevenir,
+prevencion}
 
-Defensa = {'defensa','armas','químicas','destrucción','prohibicion','militar','militares','fuerzas','armadas','bacteriológicas',
-'bacteriológica','biológica','biológicas','toxínicas','estratégicas','estratégica','estrategia','control','fuerza','aérea','naval',
-'institucionalidad'}
+Defensa = {defensa,armas,quimicas,destruccion,prohibicion,militar,militares,fuerzas,armadas,bacteriologicas,
+bacteriologica,biologica,biologicas,toxinicas,estrategicas,estrategica,estrategia,control,fuerza,aerea,naval,
+institucionalidad}
 
-Hacienda_y_relaciones_exteriores = {'banco central','banco','central','tributaria','impuestos','inversión','hacienda','comercio','tratado','tratados',
-'transpacífico','mundial','internacional','estadístico','OPS','extradición','gastos','cuentas','reservas','reservados',
-'socioeconómica','gratuidad','financiamiento','finaciero','financiera','IPOM','bienes','mercado','IEF','IPC','bonificación',
-'finanzas','productividad','emprendimiento','aduana','aduanas','aduaneros','pago','sobregiros','cuenta','banco','bancos','bancarias',
-'moneda','monetaria','inversiones','estabilidad','quiebra','deuda','deudas','mercado','mercados','cuentas','económia','económica',
-'económicas','económicos','económico','convención','convenciones','convenio','tratado','acuerdo','asociación','asociaciones',
-'integral','estatuto','unión','naciones','ONU','relaciones','exteriores','República'}
+Hacienda_y_relaciones_exteriores = {banco central,banco,central,tributaria,impuestos,inversion,hacienda,comercio,tratado,tratados,
+transpacifico,mundial,internacional,estadistico,OPS,extradicion,gastos,cuentas,reservas,reservados,
+socioeconomica,gratuidad,financiamiento,finaciero,financiera,IPOM,bienes,mercado,IEF,IPC,bonificacion,
+finanzas,productividad,emprendimiento,aduana,aduanas,aduaneros,pago,sobregiros,cuenta,banco,bancos,bancarias,
+moneda,monetaria,inversiones,estabilidad,quiebra,deuda,deudas,mercado,mercados,cuentas,economia,economica,
+economicas,economicos,economico,convencion,convenciones,convenio,tratado,acuerdo,asociacion,asociaciones,
+integral,estatuto,union,naciones,ONU,relaciones,exteriores,Republica}
