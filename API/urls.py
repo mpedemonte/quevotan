b1d589d332a64dff272d220b7f77c92b80653752
Nicolas <nrivas2017@alu.uct.cl>
2019-09-24 15:21:55 -0300

    Nueva API con flask

--- API/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from customer.views import CustomerViewSet
- 
-router = DefaultRouter()
-router.register('customers', CustomerViewSet, 'Customer')
-urlpatterns = router.urls
--- API/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from customer.models import Customer
-from customer.serializers import CustomerSerializer
-
-class CustomerViewSet(viewsets.ModelViewSet):
-    model = Customer
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
