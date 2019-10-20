f88da05fdfa4b8ef6283fb013b799b6e75e2fc03
Nicolas <nrivas2017@alu.uct.cl>
2019-10-20 11:24:15 -0300

    copia bd a carpeta api

--- API/flask_1.py
+++ API/flask_1.py
@@ -1,18 +1,28 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
-def index():
-    if (request.method == "POST"):
-        some_json = request.get_json()
-        return jsonify({"you sent": some_json}), 201
-    else:
-        return jsonify({"about":"Hello World!"})
+#@app.route("/", methods=["GET","POST"])
+#def index():
+#    if (request.method == "POST"):
+#        some_json = request.get_json()
+#        return jsonify({"you sent": some_json}), 201
+#    else:
+#        return jsonify({"about":"Hello World!"})
+#
+#@app.route("/multi/<int:num>", methods=["GET"])
+#def get_multiply10(num):
+#    return jsonify({"result": num*10})
 
-@app.route("/multi/<int:num>", methods=["GET"])
-def get_multiply10(num):
-    return jsonify({"result": num*10})
+api = Api(app)
+
+class Hello(Resource):
+    def get(self, name):
+        return {"Hello":name}
+
+
+api.add_resource(Hello,'/hello/<name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
